60942e6735b841596ef160cbc5d423cd599671c3
Graeme Porteous <graeme@rgbp.co.uk>
2014-01-25 17:39:30 +0000

    speed up glance view

--- app/controllers/admin/glance_controller.rb
+++ app/controllers/admin/glance_controller.rb
@@ -1,9 +1,7 @@
 class Admin::GlanceController < AdminController
 	def index
 		@observations = Patient.all.inject([]) do |data, patient|
-			patient.observations.each do |obs|
-				data.push(obs) if obs.measurements.any? {|k,v| v.nil?}
-			end
+			data += patient.observations.incomplete
 			data
 		end
 	end
