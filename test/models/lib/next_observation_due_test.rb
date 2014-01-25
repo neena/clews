ad5049db701eaf022f1b5880c00944db85f4fbe0
Sam Knight <sam@samknight.co.uk>
2014-01-25 15:58:26 +0000

    Fix tests

--- test/models/lib/next_observation_due_test.rb
+++ test/models/lib/next_observation_due_test.rb
@@ -5,28 +5,28 @@ class NextObservationDueTest < ActiveSupport::TestCase
   test '#calculate should add 12 hours to priority 0' do
     Delorean.time_travel_to(DateTime.parse('2014-01-01 00:00:00')) do
       due = NextObservationDue.calculate Time.now, 0
-      assert DateTime.parse('2014-01-01 12:00:00'), due
+      assert_equal '2014-01-01 12:00:00', due.strftime('%Y-%m-%d %H:%M:%S')
     end
   end
 
   test '#calculate should add 6 hours to priority 1' do
     Delorean.time_travel_to(DateTime.parse('2014-01-01 00:00:00')) do
       due = NextObservationDue.calculate Time.now, 1
-      assert DateTime.parse('2014-01-01 06:00:00'), due
+      assert_equal '2014-01-01 06:00:00', due.strftime('%Y-%m-%d %H:%M:%S')
     end
   end
 
   test '#calculate should add 1 hour to priority 2' do
     Delorean.time_travel_to(DateTime.parse('2014-01-01 00:00:00')) do
-      due = NextObservationDue.calculate Time.now, 1
-      assert DateTime.parse('2014-01-01 01:00:00'), due
+      due = NextObservationDue.calculate Time.now, 2
+      assert_equal '2014-01-01 01:00:00', due.strftime('%Y-%m-%d %H:%M:%S')
     end
   end
 
   test '#calculate should add 1 minute to priority 3' do
     Delorean.time_travel_to(DateTime.parse('2014-01-01 00:00:00')) do
-      due = NextObservationDue.calculate Time.now, 1
-      assert DateTime.parse('2014-01-01 00:01:00'), due
+      due = NextObservationDue.calculate Time.now, 3
+      assert_equal '2014-01-01 00:01:00', due.strftime('%Y-%m-%d %H:%M:%S')
     end
   end
 
