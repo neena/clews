8c463eff2ada9af3c2ad6c76f42597a07fe51ec0
jubjubbird01 <neenadugar97@gmail.com>
2014-10-12 19:01:21 +0100

    Files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716185326) do
+ActiveRecord::Schema.define(version: 20140711144423) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -77,27 +77,6 @@ ActiveRecord::Schema.define(version: 20140716185326) do
   end
 
   add_index "reminders", ["patient_id"], name: "index_reminders_on_patient_id"
-
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "rank"
-    t.string   "given_name"
-    t.string   "surname"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "wards", force: true do |t|
     t.string   "name"
