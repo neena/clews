aece80b88e3ddff8b2e55914d0dd80eac23ca3e8
jubjubbird01 <junkemail273@gmail.com>
2014-10-29 13:21:39 +0000

    Improved NextObservationDue Library

--- lib/next_observation_due.rb
+++ lib/next_observation_due.rb
@@ -1,11 +1,9 @@
 class NextObservationDue
   FREQUENCY = {
-    '0' => 12.hours,
-    '1' => 6.hours,
-    '2' => 1.hour,
-    '3' => 1.minute
+    '0' => [8.hours, 20.hours],
+    '1' => [2.hours, 8.hours, 14.hours, 20.hours],
+    '2' => (0..23).to_a.map(&:hours)
   }
-
   # Public: Calculates when next Observation is due by
   #
   # last_observed - DateTime of last Observation
@@ -14,6 +12,10 @@ class NextObservationDue
   # Returns DateTime of next Observation
   def self.calculate(last_observed, rating)
     raise ArgumentError, 'Rating must be within range' if FREQUENCY[rating.to_s].nil?
-    last_observed + FREQUENCY[rating.to_s]
+    if rating == 3
+      DateTime.now
+    else
+      FREQUENCY[rating.to_s].map{|h| DateTime.now.midnight + h}.find {|d| d > DateTime.now}
+    end
   end
 end
